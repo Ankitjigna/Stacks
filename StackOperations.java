56ef9a490e161a01ee81a29c2ed488a54a05f228
Ankit Pal <114059115+Ankitjigna@users.noreply.github.com>
2025-10-13 22:12:16 +0530

    stack using LinkedList

--- StackOperations.java
+++ StackOperations.java
@@ -1,26 +1,89 @@
 import java.util.*;
 public class StackOperations{
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    // static class Stack{
+    //     static ArrayList<Integer> list = new ArrayList<>();
+    //     public static boolean isEmpty(){
+    //         return list.size() == 0;
+    //     }
+
+    //     // push
+    //     public static void push(int data ){
+    //         list.add(data);
+    //     }
+
+    //     // pop
+    //     public static int  pop(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top = list.get(list.size()-1);
+    //         list.remove(list.size()-1);
+    //         return top;
+    //     }
+
+    //     //p peak
+    //     public static int peek(){
+    //          if(isEmpty()){
+    //             return -1;
+    //         }
+    //         return list.get(list.size()-1);
+    //     }
+    // }
+    // public static void main(String args[]){
+    //     Stack s = new Stack();
+    //     s.push(1);
+    //     s.push(2);
+    //     s.push(3);
+    //     s.push(4);
+        
+    //     while(!s.isEmpty()){
+    //         System.out.println(s.peek());
+    //         s.pop();
+    //     }
+
     static class Stack{
-        static ArrayList<Integer> list = new ArrayList<>();
+        static Node head = null;
+
         public static boolean isEmpty(){
-            return list.size() == 0;
+            return head == null;
         }
 
         // push
         public static void push(int data ){
-            list.add(data);
+            Node newNode = new Node(data);
+            
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
         }
 
         // pop
         public static int  pop(){
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
+            if(isEmpty()){
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
             return top;
         }
 
         //p peak
         public static int peek(){
-            return list.get(list.size()-1);
+             if(isEmpty()){
+                return -1;
+            }
+            return head.data;
         }
     }
     public static void main(String args[]){
@@ -33,6 +96,6 @@ public class StackOperations{
         while(!s.isEmpty()){
             System.out.println(s.peek());
             s.pop();
-        }
     }
+}
 }
