f387f47c6e060ab7edf98251da45e3b5813af871
Ankit Pal <114059115+Ankitjigna@users.noreply.github.com>
2025-10-19 08:56:33 +0530

    Next Greater Element in the Left hand side

--- NextGreaterElement.java
+++ NextGreaterElement.java
@@ -5,9 +5,11 @@ public class NextGreaterElement{
         Stack<Integer> s = new Stack<>();
         int nxtGreater[] = new int[arr.length];
 
-        for(int i=arr.length-1; i>=0; i--){
+        // for(int i=arr.length-1; i>=0; i--){      // Next Greater Element in Right hand side
+        for(int i=0; i<=arr.length-1; i++){         // Next Greater Element in Left hand side
             //while
-            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){   
+            
                 s.pop();
             }
 
